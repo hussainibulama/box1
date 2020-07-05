239551df677a019169f7860c719658d0261e98aa
hussainibulama <43305254+hussainibulama@users.noreply.github.com>
2020-07-05 22:45:50 +0100

    Added my project

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.57e540344ca16aad0ff49be7d8ed6497.js"
+  "./precache-manifest.b498fc87a609a5737e5fc29f180e2628.js"
 );
 
 self.addEventListener('message', (event) => {
