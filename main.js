a378d32f536f5a320b7f0cb40b248db9eba033c0
hussainibulama <43305254+hussainibulama@users.noreply.github.com>
2020-09-15 21:32:31 +0100

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ app.use(express.json());
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "20!$@Rem14",
   database: "box1",
 });
 db.connect(function (err) {
